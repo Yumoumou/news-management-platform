1a6af3ae105a49d95224548cce0b28156ebcec22
hujiewen <yumm2002@gmail.com>
2025-03-30 11:33:59 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootbigevent/SpringbootBigeventApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootbigevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBigeventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBigeventApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootbigevent/SpringbootBigeventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootbigevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBigeventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
