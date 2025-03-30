4195fc2b75e8a37e72bfd45c443ee68df2657de2
hujiewen <yumm2002@gmail.com>
2025-03-30 15:50:01 +0800

    parameter validation in register api

--- src/main/java/com/example/springbootbigevent/controller/UserController.java
+++ src/main/java/com/example/springbootbigevent/controller/UserController.java
@@ -3,19 +3,22 @@ package com.example.springbootbigevent.controller;
 import com.example.springbootbigevent.pojo.Result;
 import com.example.springbootbigevent.pojo.User;
 import com.example.springbootbigevent.service.UserService;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
-    public Result registerUser(String username, String password) {
+    public Result registerUser(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password) {
         // Look up database (whether username exists)
         User user = userService.findByUsername(username);
         if (user == null) {
--- src/main/java/com/example/springbootbigevent/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/springbootbigevent/service/impl/UserServiceImpl.java
@@ -26,4 +26,5 @@ public class UserServiceImpl implements UserService {
         // Register
         userMapper.add(username, encryptedPwd);
     }
+
 }
